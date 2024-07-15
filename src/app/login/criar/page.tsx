791b2aa7818791218c03412889beae61acd2cf3d
Higor Skowronski De Souza - External <e-skowronski@weg.net>
2024-07-15 11:34:11 -0300

    feat: complete register user

--- src/app/login/criar/page.tsx
+++ src/app/login/criar/page.tsx
@@ -1,7 +1,16 @@
+import { LoginCriarForm } from '@/components/Login/login-criar-form';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Crie sua Conta',
+  description: 'Crie sua conta no site Dogs.',
+};
+
 export default async function CriarPage() {
   return (
-    <main>
-      <h1>Criar Page</h1>
-    </main>
+    <div className="animeLeft">
+      <h1 className="title">Cadastra-se</h1>
+      <LoginCriarForm />
+    </div>
   );
 }
