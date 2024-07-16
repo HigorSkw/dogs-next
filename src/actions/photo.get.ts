9e57e2c83327b614fe2ffb51162f643db84904ca
Higor Skowronski De Souza - External <e-skowronski@weg.net>
2024-07-16 10:59:29 -0300

    feat: complete photo page

---
+++ src/actions/photo.get.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { PHOTOS_GET, PHOTO_GET } from '@/functions/api';
+import apiError from '@/functions/api.error';
+import { Photo } from './photos-get';
+
+type IComment = {
+  comment_ID: string;
+  comment_post_ID: string;
+  comment_author: string;
+  comment_content: string;
+};
+
+export type PhotoData = {
+  photo: Photo;
+  comments: IComment[];
+};
+
+export default async function PhotoGet(id: string) {
+  try {
+    const { url } = PHOTO_GET(id);
+    const response = await fetch(url, { next: { revalidate: 60, tags: ['photos', 'comment'] } });
+
+    const data = (await response.json()) as PhotoData;
+    if (!response.ok) throw new Error('Erro ao pegar a foto.');
+    return { data, ok: true, error: '' };
+  } catch (error) {
+    return apiError(error);
+  }
+}
