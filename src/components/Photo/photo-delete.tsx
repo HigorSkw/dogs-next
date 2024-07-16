fac2294ba0e1278f2b71ac4bddc2728685c0df45
Higor Skowronski De Souza - External <e-skowronski@weg.net>
2024-07-16 11:16:56 -0300

    feat photo delete actions

--- src/components/Photo/photo-delete.tsx
+++ src/components/Photo/photo-delete.tsx
@@ -1,3 +1,20 @@
-export default async function PhotoDelete({ id }: { id: string }) {
-  return <button>Deletar: {id}</button>;
+'use client';
+
+import photoDeleteFunction from '@/actions/photo-delete';
+import { useState } from 'react';
+
+export default function PhotoDelete({ id }: { id: string }) {
+  const [loading, setLoading] = useState(false);
+
+  async function handleClick() {
+    setLoading(true);
+    const confirm = window.confirm('Tem certeza que deseja deletar?');
+
+    if (confirm) {
+      await photoDeleteFunction(id);
+    }
+    setLoading(false);
+  }
+
+  return <>{loading ? <button disabled>Deletar</button> : <button onClick={handleClick}>Deletar: {id}</button>}</>;
 }
