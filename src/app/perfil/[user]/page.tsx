42856b7d98db439b39b7387249a87417e0dd7819
Higor Skowronski De Souza - External <e-skowronski@weg.net>
2024-07-16 16:03:04 -0300

    fix: some corrections

--- src/app/perfil/[user]/page.tsx
+++ src/app/perfil/[user]/page.tsx
@@ -1,7 +1,15 @@
+import PhotosGet from '@/actions/photos-get';
+import Feed from '@/components/Feed/feed';
+
 export default async function PerfilUserPage({ params }: { params: { user: string } }) {
+  const { data } = await PhotosGet({ user: params.user });
+
+  if (!data) return null;
+
   return (
-    <main>
-      <h1>Perfil User Page - {params.user}</h1>
-    </main>
+    <section className="container mainContainer">
+      <h1 className="title">{params.user}</h1>
+      <Feed photos={data} user={params.user} />
+    </section>
   );
 }
